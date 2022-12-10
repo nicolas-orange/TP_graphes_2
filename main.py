716485ba018697498b051c6ebe0b9a46a243e003
nicolas-orange <nicolas.moreau@orange.com>
2022-12-10 23:08:25 +0100

    affichage complet 3 cartes routes

--- main.py
+++ main.py
@@ -8,98 +8,160 @@ import networkx as nx
 import cv2
 from networkx import Graph
 
-bck_img = cv2.imread('./carte.jpg')
-
-map_data = pd.read_csv('./TP2_position.csv')
-graph_data = pd.read_csv('./TP2_liaison.csv')
-
-print(map_data.head())
-print(graph_data.head())
-
 
 def toarray(fichier):
-    myarray = np.loadtxt(fichier, delimiter=";", dtype=str)
-    return myarray
-
-
-arraypositions = toarray('./TP2_position.csv')
-arrayedges = toarray('./TP2_liaison.csv')
+  myarray = np.loadtxt(fichier, delimiter=";", dtype=str)
+  return myarray
 
 
 def chop_header(myarray, a_un_entete):
-    newarray = np.stack(myarray)  # recopie du array myarray
-    if a_un_entete:
-        print("suppression du premier element du tableau  : ", myarray[0])
-        newarray=myarray[1:] # recopie de myarray sans la premiere ligne
-
-    else:
-        newarray = myarray
-    print("le nouveau tableau est maintenant : ", newarray[0:1])
-    return newarray
-
-
-chopped_map_data = chop_header(arraypositions, True)
-chopped_graph_data = chop_header(arrayedges, True)
-
-print("liste des positions : ", chopped_map_data[0:2])
-print("mon array est maintenant : ", chopped_graph_data[0:2])
+  newarray = np.stack(myarray)  # recopie du array myarray
+  if a_un_entete:
+    print("suppression du premier element du tableau  : ", myarray[0])
+    newarray = myarray[1:]  # recopie de myarray sans la premiere ligne
+  
+  else:
+    newarray = myarray
+  # print("le nouveau tableau est maintenant : ", newarray[0:1])
+  return newarray
 
 
-def construitgraphe(myarray):
-    nombreelements = len(myarray)
-    # print("nombre d'éléments:", nombreelements)
-
-    graph: Graph = nx.Graph()
-
+def construitgraphe(myarray, routes):
+  nombreelements = len(myarray)
+  # print("nombre d'éléments:", nombreelements)
+  
+  graph: Graph = nx.Graph()
+  colors : Dict={'a': 'red', 'd': 'green'}
+  if routes == 'a' :
     for i in range(nombreelements):
-        # print("intégration de : ", (myarray[i][0]), (myarray[i][1]), (myarray[i][2]))
+      if myarray[i][2] == 'a':
         graph.add_edge(myarray[i][0], myarray[i][1], type=myarray[i][2], duree=float(myarray[i][3]),
-                       cout=float(myarray[i][4]))
+                       cout=float(myarray[i][4]), color='red')
+        graph.add_node(myarray[i][0])
+        graph.add_node(myarray[i][1])
+      else:
+        graph.add_node(myarray[i][0])
+        graph.add_node(myarray[i][1])
+    return graph
+  elif routes == 'd':
+    for i in range(nombreelements):
+      if myarray[i][2] == 'd':
+        graph.add_edge(myarray[i][0], myarray[i][1], type=myarray[i][2], duree=float(myarray[i][3]),
+                       cout=float(myarray[i][4]), color='green')
+        graph.add_node(myarray[i][0])
+        graph.add_node(myarray[i][1])
+      else:
+        graph.add_node(myarray[i][0])
+        graph.add_node(myarray[i][1])
+    return graph
+  else:
+    for i in range(nombreelements):
+      if myarray[i][2] == 'd':
+        graph.add_edge(myarray[i][0], myarray[i][1], type=myarray[i][2], duree=float(myarray[i][3]),
+                       cout=float(myarray[i][4]), color='green')
+        graph.add_node(myarray[i][0])
+        graph.add_node(myarray[i][1])
+      elif myarray[i][2] == 'a':
+        graph.add_edge(myarray[i][0], myarray[i][1], type=myarray[i][2], duree=float(myarray[i][3]),
+                       cout=float(myarray[i][4]), color='red')
+        graph.add_node(myarray[i][0])
+        graph.add_node(myarray[i][1])
+      else:
+        graph.add_node(myarray[i][0])
+        graph.add_node(myarray[i][1])
     return graph
 
 
 def construitpos(myarray):
-    posvilles: dict[str, Any] = {}
-    nombreelements = len(myarray)
-    print("nombre d'éléments:", nombreelements)
-
-    for idxville in range(nombreelements):
-        # print("intégration de : ", (myarray[i][0]), (myarray[i][1]), (myarray[i][2]))
-        posvilles[(myarray[idxville][0])] = (int(myarray[idxville][1]), int(myarray[idxville][2]))
-
-    return posvilles
-
-posv = construitpos(chopped_map_data)
-
-print("positions de Lille : ", posv['Lille'])
-
-G = construitgraphe(chopped_graph_data)
-
-print("liste des noeuds de G : ", dict(G.nodes.items()))
-print("contenu du graph G : ", dict(G.edges.items()))
-
-# on commence par afficher l'image de fond
-plt.imshow(bck_img)
-
-# utilisation d'une position bidon pour faire un premier affichage
-pos = nx.spring_layout(G, seed=13, k=1.666)  # positions for all nodes - seed for reproducibility
-
-nx.draw_networkx_nodes(G, pos=posv)
-nx.draw_networkx_edges(G, pos=posv, width=2)
-# on dessine les labels :
-# nx.draw_networkx_labels(G, pos=pos, horizontalalignment='center')
-
-# modification de la position entre deux affichages :
-
-# nx.draw_networkx_labels(G, pos=newpos, labels=labels, verticalalignment='center')
-
-# nx.draw_networkx_edge_labels(g, pos=pos)
+  posvilles: dict[str, Any] = {}
+  nombreelements = len(myarray)
+  # print("nombre d'éléments:", nombreelements)
+  
+  for idxville in range(nombreelements):
+    # print("intégration de : ", (myarray[i][0]), (myarray[i][1]), (myarray[i][2]))
+    posvilles[(myarray[idxville][0])] = (int(myarray[idxville][1]), int(myarray[idxville][2]))
+  
+  return posvilles
 
 
-# preparation de l'affichage secondaire ordonné:
-# ax = plt.subplots()
-# ax.margins(0.08)
-plt.axis("on")
+def affichage(graph, bck_img):
+  plt.figure(str(graph))
+  plt.rcParams["figure.figsize"] = (60, 15)
+  
+  # on commence par afficher l'image de fond
+  plt.imshow(bck_img)
+  
+  # utilisation d'une position bidon pour faire un premier affichage
+  # pos = nx.spring_layout(graph, seed=13, k=1.666)  # positions for all nodes - seed for reproducibility
+  
+  nx.draw_networkx_nodes(graph, pos=posv)
+  # on dessine les labels des noeuds:
+  nx.draw_networkx_labels(graph, pos=posv)
+  colors=[]
+  for edge in graph.edges:
+    colors.append(graph.edges[edge]['color'])
+  # colors = [graph.edges[edge]['color'] for edge in graph.nodes]
+  nx.draw_networkx_edges(graph, pos=posv, width=2, edge_color=colors)
 
-# plt.tight_layout()
-plt.show()
+  
+  
+  
+  # modification de la position entre deux affichages :
+  
+  # nx.draw_networkx_labels(graph, pos=newpos, labels=labels, verticalalignment='center')
+  
+  # nx.draw_networkx_edge_labels(g, pos=pos)
+  
+  # preparation de l'affichage secondaire ordonné:
+  # ax = plt.subplots()
+  # ax.margins(0.08)
+  plt.axis("off")
+
+  
+  # plt.tight_layout()
+
+
+
+################ main ##################
+if __name__ == '__main__':
+  # read string from user
+  
+  image = cv2.imread('./carte.jpg')
+  arraypositions = toarray('./TP2_position.csv')
+  arrayedges = toarray('./TP2_liaison.csv')
+  
+  r_entete = 'Yes'
+  # r_entete = input('Les fichiers de données possedent t ils un entete (y/n) (Y)? : ' )
+  if r_entete in ['n', 'N', 'no', 'NO', 'No', 'non', 'Non', 'NON']:
+    a_un_entete = False
+  else:
+    a_un_entete = True
+  
+  # print(map_data.head())
+  # print(graph_data.head())
+  chopped_map_data = chop_header(arraypositions, a_un_entete)
+  chopped_graph_data = chop_header(arrayedges, a_un_entete)
+  
+  # print("liste des positions : ", chopped_map_data[0:2])
+  # print("mon array est maintenant : ", chopped_graph_data[0:2])
+  
+  posv = construitpos(chopped_map_data)
+  
+  # print("positions de Lille : ", posv['Lille'])
+  
+  G = construitgraphe(chopped_graph_data, 'tout')
+  Ga = construitgraphe(chopped_graph_data, 'a')
+  Gd = construitgraphe(chopped_graph_data, 'd')
+  
+  
+  # print("liste des noeuds de G : ", dict(G.nodes.items()))
+  # print("liste des routes du graph G : ", dict(G.edges.items()))
+
+  affichage(G, image)
+  affichage(Ga, image)
+  affichage(Gd, image)
+
+  for edge in G.edges:
+    print(G.edges[edge]['color'])
+    
+  plt.show()
